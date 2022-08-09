1e1d49aaad071c398f874c62144f8235b0873b60
Caio Carvalho <caio.cmalves@gmail.com>
2022-08-09 19:29:34 -0300

    atualização com a aula 172 do curso

--- src/Exception/application/App.java
+++ src/Exception/application/App.java
@@ -5,6 +5,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+    
+        method1();
+
+        System.out.println("End of program.");
+
+    }
+    
+    public static void method1() {
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END***");
+
+    }
+
+    public static void method2() {
+        System.out.println("***METHOD2 START***");
         Scanner sc = new Scanner(System.in);
 
         try{ 
@@ -14,14 +30,16 @@ public class App {
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid Position!");
+            e.printStackTrace();
+            sc.next();
         }
         catch (InputMismatchException e) {
             System.out.println("Input error!");
         }
 
-        System.out.println("End of program.");
 
         sc.close();
+        System.out.println("***METHOD2 END***");
     }
     
 }
