c7cf138316a822f55ddd4b8d1aa93cf3478845a5
Caio Carvalho <caio.cmalves@gmail.com>
2022-08-12 14:45:56 -0300

    aula 3 sobre tratamento de arquivos

---
+++ src/Arquivos/application/Aula3.java
@@ -0,0 +1,24 @@
+package Arquivos.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Aula3 {
+    public static void main(String[] args) {
+        
+        String path = "C:\\Users\\015056631\\Documents\\MeusProjetos\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+}
